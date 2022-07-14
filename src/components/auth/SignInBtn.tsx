055096cf4fb089a2f4fa6c1caff13471e83de893
bytesavvie <luckystan0525@gmail.com>
2022-07-14 20:04:24 +0200

    Fix: margin on sign in and sign up buttons

--- src/components/auth/SignInBtn.tsx
+++ src/components/auth/SignInBtn.tsx
@@ -14,7 +14,7 @@ const SignInBtn = () => {
     <button
       onClick={() => doLogIn()}
       disabled={isLoading}
-      className='bg-purple-700 hover:bg-purple-500 text-white font-bold py-2 px-4 rounded-full ml-2 mt-3'
+      className='bg-purple-700 hover:bg-purple-500 text-white font-bold py-2 px-4 rounded-full mt-3'
     >
       Sign In
     </button>

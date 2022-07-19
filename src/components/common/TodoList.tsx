b3b67ae606adfb269b5895892a993e646b42540a
bytesavvie <luckystan0525@gmail.com>
2022-07-19 18:42:39 +0200

    Fix: favicon and title for list/tasks

--- src/components/common/TodoList.tsx
+++ src/components/common/TodoList.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Link from 'next/link';
 import { query } from 'thin-backend';
 import { useQuery, useIsLoggedIn } from 'thin-backend-react';
@@ -26,93 +27,109 @@ function TodoList() {
 
   return (
     (isLoggedIn && (
-      <div className='w-full h-screen flex flex-col justify-start items-center p-4'>
-        <Link href='/todos'>
-          <a className='text-[3rem] lg:text-[5rem] md:text-[5rem] font-extrabold text-gray-700'>
-            <span className='text-indigo-500'>Next</span> Day
-          </a>
-        </Link>
-        <UserStatus />
-        <h3 className='text-[2rem] lg:text-[3rem] md:text-[3rem] font-extrabold text-gray-700 w-full lg:w-2/3 '>
-          Task Lists:
-        </h3>
+      <div>
+        <Head>
+          <title>Next Day | Lists</title>
+          <link rel='icon' href='/favicon.ico' />
+        </Head>
+        <div className='w-full h-screen flex flex-col justify-start items-center p-4'>
+          <Link href='/todos'>
+            <a className='text-[3rem] lg:text-[5rem] md:text-[5rem] font-extrabold text-gray-700'>
+              <span className='text-indigo-500'>Next</span> Day
+            </a>
+          </Link>
+          <UserStatus />
+          <h3 className='text-[2rem] lg:text-[3rem] md:text-[3rem] font-extrabold text-gray-700 w-full lg:w-2/3 '>
+            Task Lists:
+          </h3>
 
-        <div className='grid grid-cols-1 grid-rows-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-3 mt-3 pt-3 w-full lg:w-3/4 md:w-full'>
-          {todo_lists?.map(
-            (todo_list) => (
-              (count = 0),
-              (
-                <div className='flex flex-col justify-center items-center rounded shadow-xl border-2 border-gray-500 h-full w-11/12 p-1 m-auto'>
-                  <Link href={`/todos/${todo_list.id}`} key={todo_list.id}>
-                    <div className='bg-slate-100 cursor-pointer flex flex-col justify-center items-start text-left h-full w-full p-6 rounded'>
-                      <h2 className='text-2xl text-gray-700 font-bold'>
-                        {todo_list.name}
-                      </h2>
+          <div className='grid grid-cols-1 grid-rows-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-3 mt-3 pt-3 w-full lg:w-3/4 md:w-full'>
+            {todo_lists?.map(
+              (todo_list) => (
+                (count = 0),
+                (
+                  <div className='flex flex-col justify-center items-center rounded shadow-xl border-2 border-gray-500 h-full w-11/12 p-1 m-auto'>
+                    <Link href={`/todos/${todo_list.id}`} key={todo_list.id}>
+                      <div className='bg-slate-100 cursor-pointer flex flex-col justify-center items-start text-left h-full w-full p-6 rounded'>
+                        <h2 className='text-2xl text-gray-700 font-bold'>
+                          {todo_list.name}
+                        </h2>
 
-                      <ul className='list-none list-inside divide-y-2 divide-dotted divide-indigo-400 w-full py-2'>
-                        {todos?.map((todo) =>
-                          todo.todoListsId === todo_list.id
-                            ? (count++,
-                              (
-                                <li
-                                  className='flex flex-row justify-between py-1'
-                                  key={todo.id}
-                                >
-                                  <div>
-                                    {todo.isCompleted ? '✅' : '❌'}{' '}
-                                    <span className='font-semibold'>
-                                      {todo.name}
-                                    </span>
-                                  </div>
-                                  <div className='font-semibold'>
-                                    {todo.duration} min
-                                  </div>
-                                </li>
-                              ))
-                            : null
-                        )}
-                      </ul>
-                      {count === 0 ? <p>The list is empty.</p> : null}
-                    </div>
-                  </Link>
-                  <div className='flex flex-row justify-between items-center h-full w-full p-1'>
-                    <div className='flex flex-row justify-start gap-2'>
-                      <PlayPomo id={todo_list.id} />
-                      <ResetPomo id={todo_list.id} />
-                    </div>
-                    <div className='flex flex-row justify-end gap-2'>
-                      <EditList
-                        id={todo_list.id}
-                        name={todo_list.name.replace(/ /g, '\u00a0')}
-                      />
-                      <DeleteList listId={todo_list.id} />
+                        <ul className='list-none list-inside divide-y-2 divide-dotted divide-indigo-400 w-full py-2'>
+                          {todos?.map((todo) =>
+                            todo.todoListsId === todo_list.id
+                              ? (count++,
+                                (
+                                  <li
+                                    className='flex flex-row justify-between py-1'
+                                    key={todo.id}
+                                  >
+                                    <div>
+                                      {todo.isCompleted ? '✅' : '❌'}{' '}
+                                      <span className='font-semibold'>
+                                        {todo.name}
+                                      </span>
+                                    </div>
+                                    <div className='font-semibold'>
+                                      {todo.duration} min
+                                    </div>
+                                  </li>
+                                ))
+                              : null
+                          )}
+                        </ul>
+                        {count === 0 ? <p>The list is empty.</p> : null}
+                      </div>
+                    </Link>
+                    <div className='flex flex-row justify-between items-center h-full w-full p-1'>
+                      <div className='flex flex-row justify-start gap-2'>
+                        <PlayPomo id={todo_list.id} />
+                        <ResetPomo id={todo_list.id} />
+                      </div>
+                      <div className='flex flex-row justify-end gap-2'>
+                        <EditList
+                          id={todo_list.id}
+                          name={todo_list.name.replace(/ /g, '\u00a0')}
+                        />
+                        <DeleteList listId={todo_list.id} />
+                      </div>
                     </div>
                   </div>
-                </div>
+                )
               )
-            )
-          )}
-          <NewList />
+            )}
+            <NewList />
+          </div>
+          <Link href='/'>
+            <a className='text-base text-violet-600 underline decoration-dotted underline-offset-2 cursor-pointer py-4'>
+              Go Home 🏠
+            </a>
+          </Link>
         </div>
-        <Link href='/'>
-          <a className='text-base text-violet-600 underline decoration-dotted underline-offset-2 cursor-pointer py-4'>
-            Go Home 🏠
-          </a>
-        </Link>
       </div>
     )) || (
-      <div className='w-screen h-screen flex flex-col justify-center items-center p-4'>
-        <Link href='/todos'>
-          <a className='text-[3rem] lg:text-[5rem] md:text-[5rem] font-extrabold text-gray-700'>
-            <span className='text-indigo-500'>Next</span> Day
-          </a>
-        </Link>
-        <UserStatus />
-        <Link href='/'>
-          <a className='text-base text-violet-600 underline decoration-dotted underline-offset-2 cursor-pointer py-4'>
-            Go Home 🏠
-          </a>
-        </Link>
+      <div>
+        <Head>
+          <title>Next Day</title>
+          <meta
+            name='description'
+            content='Create and manage your todo lists.'
+          />
+          <link rel='icon' href='/favicon.ico' />
+        </Head>
+        <div className='w-screen h-screen flex flex-col justify-center items-center p-4'>
+          <Link href='/todos'>
+            <a className='text-[3rem] lg:text-[5rem] md:text-[5rem] font-extrabold text-gray-700'>
+              <span className='text-indigo-500'>Next</span> Day
+            </a>
+          </Link>
+          <UserStatus />
+          <Link href='/'>
+            <a className='text-base text-violet-600 underline decoration-dotted underline-offset-2 cursor-pointer py-4'>
+              Go Home 🏠
+            </a>
+          </Link>
+        </div>
       </div>
     )
   );
--- src/pages/todos/[id].tsx
+++ src/pages/todos/[id].tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { query } from 'thin-backend';
@@ -23,86 +24,99 @@ const Todo: NextPage = () => {
 
   let count = 0;
 
+  // get current todo list with todo_list_id
+  const current_todo_list = todo_list?.find(
+    (todo_list) => todo_list.id === todo_list_id
+  );
+
   return (
     (isLoggedIn && (
-      <div className='w-full h-screen flex flex-col justify-start items-center p-4'>
-        {todo_list === null ? (
-          <div>Loading ...</div>
-        ) : (
-          todo_list.map((todo_list) => (
-            <>
-              <Link href='/todos'>
-                <a className='text-[3rem] lg:text-[5rem] md:text-[5rem] font-extrabold text-gray-700'>
-                  <span className='text-indigo-500'>Next</span> Day
-                </a>
-              </Link>
-              <h1
-                className='text-[3rem] lg:text-[5rem] md:text-[5rem] font-extrabold text-gray-700'
-                key={todo_list.id}
-              >
-                <p>{todo_list.name}</p>
-              </h1>
-            </>
-          ))
-        )}
+      <div>
+        <Head>
+          <title>Next Day | {current_todo_list?.name}</title>
+          <link rel='icon' href='/favicon.ico' />
+        </Head>
+        <div className='w-full h-screen flex flex-col justify-start items-center p-4'>
+          {todo_list === null ? (
+            <div>Loading ...</div>
+          ) : (
+            todo_list.map((todo_list) => (
+              <>
+                <Link href='/todos'>
+                  <a className='text-[3rem] lg:text-[5rem] md:text-[5rem] font-extrabold text-gray-700'>
+                    <span className='text-indigo-500'>Next</span> Day
+                  </a>
+                </Link>
+                <h1
+                  className='text-[3rem] lg:text-[5rem] md:text-[5rem] font-extrabold text-gray-700'
+                  key={todo_list.id}
+                >
+                  <p>{todo_list.name}</p>
+                </h1>
+              </>
+            ))
+          )}
 
-        <h3 className='text-[2rem] lg:text-[3rem] md:text-[3rem] font-extrabold text-gray-700 w-full lg:w-2/3 md:w-full mt-3'>
-          Tasks:
-        </h3>
+          <h3 className='text-[2rem] lg:text-[3rem] md:text-[3rem] font-extrabold text-gray-700 w-full lg:w-2/3 md:w-full mt-3'>
+            Tasks:
+          </h3>
 
-        {todos === null ? (
-          <div>Loading ...</div>
-        ) : (
-          ((count = 0),
-          (
-            <>
-              <div className='grid grid-cols-1 grid-rows-3 lg:grid-rows-1 md:grid-rows-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-3 mt-3 pt-3 w-full lg:w-3/4 md:w-full'>
-                {todos.map(
-                  (todo) => (
-                    count++,
-                    (
-                      <div className='flex flex-col rounded shadow-xl border-2 border-gray-500 h-full w-11/12 p-1 m-auto'>
-                        <div
-                          className='bg-slate-100 flex flex-col justify-center items-start text-left h-full w-full p-6 rounded '
-                          key={todo.id}
-                        >
-                          <h2 className='text-3xl text-gray-700 font-bold underline underline-offset-4 decoration-indigo-400 decoration-dashed'>
-                            {todo.name}
-                          </h2>
-                          <ul className='list-none list-inside pl-'>
-                            <li>
-                              Duration:{' '}
-                              <span className='font-semibold'>
-                                {todo.duration} min
-                              </span>
-                            </li>
-                            <li>Completed: {todo.isCompleted ? '✅' : '❌'}</li>
-                          </ul>
+          {todos === null ? (
+            <div>Loading ...</div>
+          ) : (
+            ((count = 0),
+            (
+              <>
+                <div className='grid grid-cols-1 grid-rows-3 lg:grid-rows-1 md:grid-rows-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-3 mt-3 pt-3 w-full lg:w-3/4 md:w-full'>
+                  {todos.map(
+                    (todo) => (
+                      count++,
+                      (
+                        <div className='flex flex-col rounded shadow-xl border-2 border-gray-500 h-full w-11/12 p-1 m-auto'>
+                          <div
+                            className='bg-slate-100 flex flex-col justify-center items-start text-left h-full w-full p-6 rounded '
+                            key={todo.id}
+                          >
+                            <h2 className='text-3xl text-gray-700 font-bold underline underline-offset-4 decoration-indigo-400 decoration-dashed'>
+                              {todo.name}
+                            </h2>
+                            <ul className='list-none list-inside pl-'>
+                              <li>
+                                Duration:{' '}
+                                <span className='font-semibold'>
+                                  {todo.duration} min
+                                </span>
+                              </li>
+                              <li>
+                                Completed: {todo.isCompleted ? '✅' : '❌'}
+                              </li>
+                            </ul>
+                          </div>
+                          <div className='flex flex-row justify-end gap-2 items-center h-full w-full p-1'>
+                            <EditTask
+                              id={todo.id}
+                              name={todo.name.replace(/ /g, '\u00a0')}
+                              duration={todo.duration}
+                            />
+                            <DeleteTask taskId={todo.id} />
+                          </div>
                         </div>
-                        <div className='flex flex-row justify-end gap-2 items-center h-full w-full p-1'>
-                          <EditTask
-                            id={todo.id}
-                            name={todo.name.replace(/ /g, '\u00a0')}
-                            duration={todo.duration}
-                          />
-                          <DeleteTask taskId={todo.id} />
-                        </div>
-                      </div>
+                      )
                     )
-                  )
-                )}
-                <NewTask todo_list_id={todo_list_id!} />
-              </div>
-              {count === 0 ? <p>The list is empty.</p> : null}
-            </>
-          ))
-        )}
+                  )}
+                  <NewTask todo_list_id={todo_list_id!} />
+                </div>
+                {count === 0 ? <p>The list is empty.</p> : null}
+              </>
+            ))
+          )}
 
-        <Link href='/todos'>
-          <a className='text-base text-violet-600 underline decoration-dotted underline-offset-2 cursor-pointer py-4'>
-            Go Back To Lists ⬅️
-          </a>
-        </Link>
+          <Link href='/todos'>
+            <a className='text-base text-violet-600 underline decoration-dotted underline-offset-2 cursor-pointer py-4'>
+              Go Back To Lists ⬅️
+            </a>
+          </Link>
+        </div>
       </div>
     )) || <></>
   );
